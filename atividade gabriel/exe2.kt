70d3363e45a4d4c5a47bd8c49b3c257dfa5fb325
mariolobo-code <mario.lobo@escola.pr.gov.br>
2026-08-27 19:34:31 -0300

    Update exe2.kt

--- atividade gabriel/exe2.kt
+++ atividade gabriel/exe2.kt
@@ -6,20 +6,20 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(saveInstanceAtate: Bunle?) {
+    override fun onCreate(saveInstanceState: Bunle?) {
           super.onCreate(savedInstanceState)
 
-          val informações = """
+          val informacoes = """
             fabricante: ${Build.MANUFACTURER}
             Modelo: ${Build.MODEL}
             Android: ${Build.VERSION.RELEASE}
             SDK: ${Build.VERSION.SDK_INT}
-            """,trimIndent()
+            """.trimIndent()
 
                 val texto = TextView(this)
-                texto.text + informaçoes
-                text0.textSize + 18f
-                text0.setPadding(32, 32, 32, 32)
+                texto.text = informaçoes
+                texto.textSize + 18f
+                texto.setPadding(32, 32, 32, 32)
 
                 setContentView(texto)
     }
