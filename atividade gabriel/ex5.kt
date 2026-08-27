ff2562bf524673317f1f7e154be8e6ddd017361c
mariolobo-code <mario.lobo@escola.pr.gov.br>
2026-08-27 19:52:18 -0300

    Update ex5.kt

--- atividade gabriel/ex5.kt
+++ atividade gabriel/ex5.kt
@@ -1,6 +1,6 @@
-packge com.example.meuapp
+package com.example.meuapp
 
-import android.os.Build
+import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
 import android.widget.EditText
@@ -9,33 +9,36 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(saveInstanceAtate: Bunle?) {
+    override fun onCreate(savedInstanceState: Bunle?) {
           super.onCreate(savedInstanceState)
 
-          val layot + LinearLayout(this).apply {
-            otientation = LinearLayout.VERTICAL
-            Gravity + Gravity.CENTER_HORIZONTAL
-            setPadding(32, 48, 32< 32)
+          val layout = LinearLayout(this).apply {
+            orientation = LinearLayout.VERTICAL
+            gravity = Gravity.CENTER_HORIZONTAL
+            setPadding(32, 48, 32, 32)
           }
 
           val valor = EditText(this).apply {
              hint = "Digite uma tarefa"
-         
+    }   
+          
      val botao = Button(this).apply {
           text = "Adicionar"
      }
+     
           val resposta = TextView(this).apply {
             text  = "nenhuma tarefa adicionada."
             textSize = 18f
           }
-          botao.setOnclilistener {
-            val terefa = valor.text.toStrining().trim()
+          
+          botao.setOnckListener {
+            val terefa = valor.text.toString().trim()
             "Digite uma tarefa antes de adicionar."
           }else {
             "Trefa adicionada: $tarefa"
             }
      }
-     layout.addView(valor)
+    layout.addView(valor)
     layout.addView(botao)
     layout.addView(resposta)
     layout.addView(layout)
